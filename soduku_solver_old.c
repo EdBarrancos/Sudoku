225d1022ab17dee8bdd96e6316f858b94099605b
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2022-03-07 13:22:16 +0000

    Fix main return int

--- soduku_solver_old.c
+++ soduku_solver_old.c
@@ -36,7 +36,7 @@ int tst_collums(int g[9][9],int y,int x);
 int tst_squares(int g[9][9],int x,int y,int x_s,int y_s);
 
 
-main()
+int main()
 {
     int r;
     int last_r = 0;
@@ -228,6 +228,8 @@ main()
             printf("\n");
         }
     }
+
+    return 0;
 }
 
 int tst_squares(int g[9][9],int x,int y,int x_s,int y_s){
